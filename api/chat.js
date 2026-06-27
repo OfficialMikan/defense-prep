244b5fa599a26fdf2a62722cdf9a5030b10066ef
OfficialMikan <mikanmnrng@gmail.com>
2026-06-27 17:31:30 +0800

    change to llama 3.1 to 3.3

--- api/chat.js
+++ api/chat.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                model: "llama-3.1-70b-versatile",
+                model: "llama-3.3-70b-versatile",
                 messages: [{ role: "user", content: prompt }],
                 temperature: 0.1,
                 max_tokens: 1000
